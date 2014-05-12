a554bb0f7c8c1e0f1d6a2be7b0a67fbe0e2de78d
Ricardo Gaviria <gaviriar@tcd.ie>
2014-05-12 11:53:02 +0100

    synchronizes with github

--- setup.sh
+++ setup.sh
@@ -1,15 +1,5 @@
 # #!/bin/bash
 # git pull and install dotfiles as well
-cd $HOME
-if [-d dotfiles.old/ ]; then
-    rm -rf dotfiles.old
-fi
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
-fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
 
 cd "$(dirname "$0")"
 git pull
