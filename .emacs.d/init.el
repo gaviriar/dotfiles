726a9db36357021f7678958f2ecfba67fe25f439
Ricardo Gaviria <ricardo.a.gaviria@gmail.com>
2016-03-02 22:22:51 +0000

    Added Ctrl-d support for multi-cursor, like sublime text :)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -117,8 +117,8 @@
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 
 ;When you want to add multiple cursors not based on continuous lines, but based on keywords in the buffer, use:
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-d") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-S-d") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 ;First mark the word, then add more cursors.
 ;To get out of multiple-cursors-mode, press `<return>` or `C-g`. The latter will first disable multiple regions before disabling multiple cursors. If you want to insert a newline in multiple-cursors-mode, use `C-j`.
