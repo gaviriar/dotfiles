368e49707ebc99eaf3243b865b82dd0200f42c9e
Ricardo Gaviria <ricardo.a.gaviria@gmail.com>
2018-01-16 10:45:24 +0100

    adding directory check before install in case project is already installed

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
 # Install rupa-z for easy directory jumping
-git clone https://github.com/rupa/z.git ~/projects/z
-chmod +x ~/projects/z/z.sh 
+if [ ! -d ~/projects/z ]; then
+    git clone https://github.com/rupa/z.git ~/projects/z
+    chmod +x ~/projects/z/z.sh 
+fi
 
 # Install virtualenv 
 sudo pip install virtualenv
@@ -14,8 +16,7 @@ sudo pip install virtualenvwrapper
 sudo pip install --upgrade pip
 
 # Create directory where envs will live
-if ! [ -d ~/.virtualenvs ] 
-then
+if [ ! -d ~/.virtualenvs ]; then
     mkdir ~/.virtualenvs
 fi
 
