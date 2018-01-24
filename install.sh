62ba346d488ba86378d9d6a0f48690829c6663b7
Ricardo Gaviria <ricardo.a.gaviria@gmail.com>
2018-01-24 09:37:03 +0100

    Update install.sh to install node and virtualenv correctly

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # install NVM 
 # NOTE: version number will change
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
 # Install rupa-z for easy directory jumping
 if [ ! -d ~/projects/z ]; then
@@ -23,4 +23,5 @@ fi
 # If this is your first time dont forget to update your .bashrc
 # personally I do this on the .extras dotfile on my machine as this is really more of a machine specific thing that I do not want to polute my .bashrc with
 # It's up to you really
-printf '\n%s\n%s\n%s' '# virtualenv' 'export WORKON_HOME=~/.virtualenvs' 'source /usr/local/bin/virtualenvwrapper.sh'>> ~/.extras
+# courtesy: http://exponential.io/blog/2015/02/10/install-virtualenv-and-virtualenvwrapper-on-ubuntu/
+printf '\n%s\n%s\n%s' '# virtualenv' 'export WORKON_HOME=~/.virtualenvs' 'source /usr/local/bin/virtualenvwrapper.sh'>> ~/.extra
